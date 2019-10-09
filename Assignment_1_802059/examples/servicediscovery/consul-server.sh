3b8f23259b518d576992b6166d4878c45c356dca
Ignacio GM <igna-94@hotmail.com>
2019-10-09 13:39:17 +0300

    right template

--- Assignment_1_802059/examples/servicediscovery/consul-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#example: -datacenter=aaltocs -data-dir=/tmp/consul -bind=0.0.0.0 -
-/home/truong/temp/consul/consul agent -enable-script-checks -datacenter=$1 -node $2 -data-dir=$3 -bind=$4 -server -dev
--- Assignment_1_802059/examples/servicediscovery/simple_list.js
+++
@@ -1,5 +0,0 @@
-var consul = require('consul')();
-consul.agent.service.list(function(err, result) {
-  if (err) throw err;
-  console.log(result);
-});
